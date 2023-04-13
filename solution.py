01114bea1dbf722f837df140f1d6d2a75c7dfd60
asitek17 <122224673+asitek17@users.noreply.github.com>
2023-04-14 01:09:30 +0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,21 @@
 import pandas as pd
 import numpy as np
 
+from scipy.stats import mannwhitneyu
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 694905952 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+def solution(*args) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+    
+    if len(args) == 1:
+        x = args[0]
+    elif len(args) == 2:
+        x = args[0]
+        y = args[1] 
+        
+    
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.03
+    return mannwhitney(x, y, alternative = "less").pvalue < alpha # Ваш ответ, True или False
